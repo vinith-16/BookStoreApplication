acf5978692ad65034fee15f78029df3bb4e8cda1
vinith-16 <vineethmuthu9@gmail.com>
2025-07-07 16:29:06 +0530

    UserPathModel.java

--- src/main/java/com/example/BookApplication/model/UserPathModel.java
+++ src/main/java/com/example/BookApplication/model/UserPathModel.java
@@ -13,32 +13,50 @@ public class UserPathModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+    private Long id;
 
-    @Column
-    String username;
+    @Column(name = "username")
+    private String username;
 
-    String password;
+    @Column(name = "password")
+    private String password;
 
-    public UserPathModel() { }
+    
+    
+	public UserPathModel() {
+		super();
+	}
 
-    public UserPathModel(Long id, String username, String password) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
+	public String getUsername() {
+		return username;
+	}
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    @Override
-    public String toString() {
-        return "LoginModel [id=" + id + ", username=" + username + ", password=" + password + "]";
-    }
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public UserPathModel(Long id, String username, String password) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+	}
+
+    
 }
